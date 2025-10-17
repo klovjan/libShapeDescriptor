e2243b5231d3051a4e1bf8bacea6bc2eaca161e9
klovjan <eivind.klo@gmail.com>
2025-10-17 11:47:22 +0200

    Randomly sample some keypoints

--- src/naive_gpu.cu
+++ src/naive_gpu.cu
@@ -1,5 +1,6 @@
 #include <shapeDescriptor/shapeDescriptor.h>
 #include <iostream>
+#include <random>
 
 #ifdef DESCRIPTOR_CUDA_KERNELS_ENABLED
 #include <cuda_runtime.h>
@@ -13,26 +14,26 @@
 // Sample rates
 // #define N_POINTCLOUD_SAMPLES 5000000
 // #define N_KEYPOINTS 500
-#define N_POINTCLOUD_SAMPLES 50
+#define N_POINTCLOUD_SAMPLES 5000
 #define N_KEYPOINTS 10
 
 // Randomness
-#define RANDOM_SEED 1
+#define RANDOM_SEED 22
 
 // Host variables
-const int h_n_pointcloud_samples = N_POINTCLOUD_SAMPLES;
+const uint32_t h_n_pointcloud_samples = N_POINTCLOUD_SAMPLES;
 
 // Device variables
 __constant__ int d_n_pointcloud_samples;  // Could just leave this as 
 
 // Define CUDA grid and block dimensions
-int dim_block = BLOCKX;
-int dim_grid = 1;
+uint32_t dim_block = BLOCKX;
+uint32_t dim_grid = 1;
 
-__global__ void calculate_descriptors(ShapeDescriptor::gpu::PointCloud device_point_cloud, ShapeDescriptor::gpu::array<int> device_keypoint_indexes, ShapeDescriptor::gpu::array<int> device_output_array) {
-    int keypoints_per_thread = N_KEYPOINTS / blockDim.x;
+__global__ void calculate_descriptors(ShapeDescriptor::gpu::PointCloud device_point_cloud, ShapeDescriptor::gpu::array<uint32_t> device_keypoint_indexes, ShapeDescriptor::gpu::array<uint32_t> device_output_array) {
+    uint32_t keypoints_per_thread = N_KEYPOINTS / blockDim.x;
 
-    for (int i = threadIdx.x * keypoints_per_thread; i < (threadIdx.x + 1) * keypoints_per_thread; i++)
+    for (uint32_t i = threadIdx.x * keypoints_per_thread; i < (threadIdx.x + 1) * keypoints_per_thread; i++)
     {
         if (i >= N_KEYPOINTS) break;
 
@@ -65,22 +66,21 @@ int main(int argc, char **argv) {
     ShapeDescriptor::cpu::PointCloud h_sampled_point_cloud = ShapeDescriptor::sampleMesh(h_mesh, N_POINTCLOUD_SAMPLES, RANDOM_SEED);
     ShapeDescriptor::gpu::PointCloud d_sampled_point_cloud = ShapeDescriptor::copyToGPU(h_sampled_point_cloud);
 
-    // Choose random subset of points from point cloud
-    std::cout << "TODO" << std::endl;
+    // Choose random subset of N_KEYPOINTS point indices from point cloud
+    std::mt19937_64 random_engine(RANDOM_SEED);
+    std::uniform_int_distribution<uint32_t> distribution(0, N_POINTCLOUD_SAMPLES - 1);
 
-    // Choose 10 first points in pointcloud (TEMPORARY)
-    ShapeDescriptor::cpu::array<int> h_keypoint_indexes(N_KEYPOINTS);
-    for (int i = 0; i < N_KEYPOINTS; i++)
-    {
-        h_keypoint_indexes[i] = i;
+    ShapeDescriptor::cpu::array<uint32_t> h_keypoint_indices(N_KEYPOINTS);
+    for (uint32_t i = 0; i < N_KEYPOINTS; i++) {
+        h_keypoint_indices[i] = distribution(random_engine);
     }
-    ShapeDescriptor::gpu::array<int> d_keypoint_indexes = ShapeDescriptor::copyToGPU(h_keypoint_indexes);
+    ShapeDescriptor::gpu::array<uint32_t> d_keypoint_indices = ShapeDescriptor::copyToGPU(h_keypoint_indices);
 
     // Set up device memory for output array
-    ShapeDescriptor::gpu::array<int> d_output_array(N_KEYPOINTS);
+    ShapeDescriptor::gpu::array<uint32_t> d_output_array(N_KEYPOINTS);
 
     // Launch kernel
-    calculate_descriptors<<<dim_grid, dim_block>>>(d_sampled_point_cloud, d_keypoint_indexes, d_output_array);
+    calculate_descriptors<<<dim_grid, dim_block>>>(d_sampled_point_cloud, d_keypoint_indices, d_output_array);
 
     // Synchronize and check if any errors occurred
     cudaError_t err = cudaDeviceSynchronize();
@@ -91,27 +91,28 @@ int main(int argc, char **argv) {
     std::cout << "Kernel finished" << std::endl;
 
     // Move output to CPU
-    ShapeDescriptor::cpu::array<int> h_output_array = ShapeDescriptor::copyToCPU(d_output_array);
+    ShapeDescriptor::cpu::array<uint32_t> h_output_array = ShapeDescriptor::copyToCPU(d_output_array);
     
     // Print output
-    for (int i = 0; i < N_KEYPOINTS; i++)
+    for (uint32_t i = 0; i < N_KEYPOINTS; i++)
     {
         std::cout << h_output_array.content[i] << std::endl;
     }
 
     // Does it match?
-    for (int i = 0; i < N_KEYPOINTS; i++)
+    for (uint32_t i = 0; i < N_KEYPOINTS; i++)
     {
-        std::cout << h_sampled_point_cloud.vertices[i].x << std::endl;
+        std::cout << h_sampled_point_cloud.vertices[h_keypoint_indices[i]].x << std::endl;
     }
     
 
     // Free the memory
-    ShapeDescriptor::free(d_output_array);
     ShapeDescriptor::free(h_mesh);
     ShapeDescriptor::free(h_sampled_point_cloud);
     ShapeDescriptor::free(d_sampled_point_cloud);
-    ShapeDescriptor::free(d_keypoint_indexes);
+    ShapeDescriptor::free(h_keypoint_indices);
+    ShapeDescriptor::free(d_keypoint_indices);
+    ShapeDescriptor::free(d_output_array);
     // ShapeDescriptor::free(d_mesh);
 
     return 0;
